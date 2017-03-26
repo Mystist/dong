f928c07b03a364cdc6a187a38a9201c5ecd1a3cc
Mystist <gjl87910lq@gmail.com>
2017-03-26 21:23:36 +0800

    forum form

---
+++ src/components/ForumForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="weui-panel">
+    <div class="weui-panel__hd">发帖</div>
+    <div class="weui-panel__bd">
+      <div class="weui-cell">
+        <div class="weui-cell__bd">
+          <input class="weui-input" type="text" placeholder="输入标题">
+        </div>
+      </div>
+      <div class="weui-cell">
+        <div class="weui-cell__bd">
+          <textarea class="weui-textarea" placeholder="输入内容" rows="3"></textarea>
+          <div class="weui-textarea-counter"><span>0</span>/200</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,14 @@ import Service from '../components/Service'
 import Neighbor from '../components/Neighbor'
 import Me from '../components/Me'
 import Forum from '../components/Forum'
+import ForumForm from '../components/ForumForm'
 
 export default new Router({
   routes: [
     { path: '/service', component: Service },
     { path: '/neighbor', component: Neighbor },
     { path: '/me', component: Me },
-    { path: '/forum', component: Forum }
+    { path: '/forum', component: Forum },
+    { path: '/forum-form', component: ForumForm }
   ]
 })
